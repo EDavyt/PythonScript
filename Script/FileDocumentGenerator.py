80828d00a3d1f18c20f6ef584e8e13ffc3481604
Esteban Davyt <esteban.davyt@combinedratio.com>
2025-05-28 09:59:45 -0300

    Removed UNecesary Coments

--- Script/FileDocumentGenerator.py
+++ Script/FileDocumentGenerator.py
@@ -27,8 +27,6 @@ def empty_output_folder(destination):
 def duplicate_fileDocument(fileTemplate, destination, uuid1gen, form_number, template_name):
         # Check if source exists
 
-        #Hola Steven
-
         if not os.path.exists(fileTemplate):
             print("File does not exist")
             return False
